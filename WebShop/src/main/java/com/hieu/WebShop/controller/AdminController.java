ebc64adf54c33ac457b57bad87a6e9caf25ef272
Đặng Hiệu <76999336+quanghieu01@users.noreply.github.com>
2023-03-01 22:49:05 +0700

    Add files via upload

---
+++ WebShop/src/main/java/com/hieu/WebShop/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.hieu.WebShop.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Transactional
+public class AdminController {
+    @RequestMapping(value = { "/admin/login" }, method = RequestMethod.GET)
+    public String login(Model model) {
+
+        return "login";
+    }
+
+    @RequestMapping(value = { "/admin/accountInfo" }, method = RequestMethod.GET)
+    public String accountInfo(Model model) {
+
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(userDetails.getPassword());
+        System.out.println(userDetails.getUsername());
+        System.out.println(userDetails.isEnabled());
+
+        model.addAttribute("userDetails", userDetails);
+        return "accountInfo";
+    }
+}
